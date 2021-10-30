912590a7a51075db1096f8f2e9e19b0d74d0a251
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2021-10-29 22:41:03 -0300

    Adicionando pagina para o genuino software e configurando a rota

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import PaginaInicial from './pages/PaginaInicial';
 import Produtos from './pages/Produtos';
 import SobreNos from './pages/SobreNos';
 import EntreEmContato from './pages/EntreEmContato';
+import GenuinoSoftware from './pages/GenuinoSoftware';
 
 const Routes = ()=>{
     return(
@@ -13,6 +14,7 @@ const Routes = ()=>{
                 <Route path="/produtos" component={Produtos} exact />
                 <Route path="/desenvolvedores" component={SobreNos} exact />
                 <Route path="/entre-em-contato" component={EntreEmContato} exact />
+                <Route path="/produtos/genuino-software" component={GenuinoSoftware} exact />
             </Switch>
         </BrowserRouter>
     )

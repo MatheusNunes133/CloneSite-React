a4f7b4eb0dc982254030b0c0c4cdf86a78f28641
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2021-10-31 23:39:24 -0300

    Testando rota

--- src/Router.js
+++ src/Router.js
@@ -11,10 +11,10 @@ const Routes = ()=>{
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/CloneSite-React" component={PaginaInicial} exact />
-                <Route path="https://matheusnunes133.github.io/CloneSite-React/produtos" component={Produtos} exact />
-                <Route path="/CloneSite-React/desenvolvedores" component={SobreNos} exact />
-                <Route path="/CloneSite-React/entre-em-contato" component={EntreEmContato} exact />
-                <Route path="/CloneSite-React/produtos/genuino-software" component={GenuinoSoftware} exact />
+                <Route path="/produtos" component={Produtos} exact />
+                <Route path="/desenvolvedores" component={SobreNos} exact />
+                <Route path="/entre-em-contato" component={EntreEmContato} exact />
+                <Route path="/produtos/genuino-software" component={GenuinoSoftware} exact />
             </Switch>
         </BrowserRouter>
     )

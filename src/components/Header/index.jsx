cbf15809e72bfac6053e8b3b45a75a23f028a01e
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2021-10-31 23:55:31 -0300

    Update nas rotas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,13 +14,13 @@ export default (props)=>{
                             <a href="/CloneSite-React" className={props.spanInicial}>Página Inicial</a>
                         </li>
                         <li>
-                            <a href="/produtos" className={props.spanProd}>Produtos</a>
+                            <a href="/CloneSite-React/#/produtos" className={props.spanProd}>Produtos</a>
                         </li>
                         <li>
-                            <a href="/entre-em-contato" className={props.spanContato}>Entre em contato</a>
+                            <a href="/CloneSite-React/#/entre-em-contato" className={props.spanContato}>Entre em contato</a>
                         </li>
                         <li> 
-                            <a href="/desenvolvedores" className={props.spanDevs}>Desenvolvedores</a>
+                            <a href="/CloneSite-React/#/desenvolvedores" className={props.spanDevs}>Desenvolvedores</a>
                         </li>
                         
                     </ul>
